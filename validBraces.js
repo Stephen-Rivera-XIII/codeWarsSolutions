47835b6370799e7bcc357075b21477e9a86ee1e6
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2022-07-20 10:23:05 -0500

    this is a disaster but it's what I tried so here we go

--- validBraces.js
+++ validBraces.js
@@ -1,6 +1,66 @@
 function validBraces(braces) {
 
-    function areEqual(array1, array2) {
+    function check(expr){
+        const holder = []
+        const openBrackets = ['(','{','[']
+        const closedBrackets = [')','}',']']
+        const tryThis= [];
+        let x = 0; // if x = 0 before the halfway point of the loop/expression
+        let j = 0
+        for (let letter of expr) { // loop trought all letters of expr
+            if(openBrackets.includes(letter)){ // if its opening bracket
+                holder.push(letter)
+                x++;
+                tryThis.push(x);
+                //j++;
+                console.log(tryThis);
+                console.log(x);
+                console.log(j);
+                // if (j > x){
+                //     return false;
+                // }
+            }else if(closedBrackets.includes(letter)){ // if its closing
+                const openPair = openBrackets[closedBrackets.indexOf(letter)] // find its pair
+                if(holder[holder.length - 1] === openPair){ 
+                    console.log(openPair)// check if that pair is the last element in the array
+                    holder.splice(-1,1) // if so, remove it
+                    x--;
+                    j++;
+                    //if(j>x){return false};
+                    console.log(x);
+                    console.log(j)
+                }else{ // if its not
+                    holder.push(letter)
+                    break // exit loop
+                }
+            }
+        }
+        for (i = 0; i < tryThis.length-1; i++)
+        {
+            var maybe = tryThis[i+1] - tryThis[i] >= tryThis[i] || tryThis[i+1] - tryThis[i] ==0;
+            console.log(tryThis[i])
+            console.log(tryThis[i+1])
+            console.log(maybe)
+            if(maybe == false){
+                return maybe
+            }
+            else{
+                i++;
+            }
+
+        }
+        console.log(j);
+        console.log(x);
+        console.log((braces.length/2))
+        console.log((holder.length === 0 && j+x==0)) //|| holder.length === 0 && j-x < 1))
+        return ((holder.length === 0 && j-x >= (braces.length/2)));//|| holder.length === 0 && j-x<1 )) // return true if length is 0, otherwise false
+    
+}
+    let preCheck = check(braces) /// true
+
+    while (preCheck==true){
+
+        function areEqual(array1, array2) {
         if (array1.length === array2.length) {
             return array1.every((element, index) => {
                 if (element === array2[index]) {
@@ -30,7 +90,7 @@ function validBraces(braces) {
     }
     console.log(openingBrackets);
     console.log(closingBrackets);
-//    let final = (closingBrackets.reverse())
+    let final = (closingBrackets.reverse())
     //console.log(final)
 
     for (let index = 0; index < closingBrackets.length; index++) {
@@ -43,7 +103,7 @@ function validBraces(braces) {
         }
     }
     //
-    //console.log(final)
+    console.log(final)
     console.log(openingBrackets);
     console.log(closingBrackets)
 
@@ -52,6 +112,7 @@ function validBraces(braces) {
         let final = (closingBrackets.reverse())
         let result2 = areEqual(openingBrackets, final);
         return result2;
+        
     }
     else if (result = true){
         return true;
@@ -60,11 +121,15 @@ function validBraces(braces) {
 
     // let result = areEqual(openingBrackets, final);
     // return result;
-
 }
-braces = "[(])"
-// braces = "(){}[]"
-//braces = "()))"
-//braces = "({[]})"
-//braces = "([{]})"
+ {
+    return false;
+}
+}
+braces = "({})[({})]"
+// braces = "[(]{)}"
+//braces = "(){}[]"
+//braces = "())({}}{()][][" 
+//braces = "())({}}{()][]["
+//braces = "()" //j and x both =1
 validBraces(braces);
