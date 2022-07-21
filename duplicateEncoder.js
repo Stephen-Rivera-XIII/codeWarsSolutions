bb6b03b333eb8d83ffd5699b3ee3478b193fe418
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2022-07-20 19:20:47 -0500

    solution

---
+++ duplicateEncoder.js
@@ -0,0 +1,14 @@
+function duplicateEncode(str) {
+    let stack = [];
+    str = str.toLowerCase();
+    for (var i=0; i<str.length; i++) {
+    if ( str.indexOf(str[i]) == str.lastIndexOf(str[i]) ) {
+            stack.push('(')
+          }  
+      if ( str.indexOf(str[i]) !== str.lastIndexOf(str[i]) ) {
+        stack.push(')')
+      }
+    }
+    let answer = stack.join('');
+    return answer;
+}
