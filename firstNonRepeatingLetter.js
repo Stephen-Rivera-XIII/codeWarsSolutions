dc5832e6afa028f1706bd21782f4fbd37b72608c
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2022-07-21 16:02:49 -0500

    final final solution

--- firstNonRepeatingLetter.js
+++ firstNonRepeatingLetter.js
@@ -1,11 +1,8 @@
 function firstNonRepeatingLetter(word) {
-    //let stack = [];
     let y = 0;
     str = word.toLowerCase();
     for (var i=0; i<str.length; i++) {
     if ( str.indexOf(str[i]) == str.lastIndexOf(str[i]) ) {
-            console.log(str[i])
-            console.log(word[i]);
             return word[i];
           }  
       if ( str.indexOf(str[i]) !== str.lastIndexOf(str[i]) ) {
@@ -14,12 +11,8 @@ function firstNonRepeatingLetter(word) {
       }
     }
     if(y>=str.length){
-        console.log(y);
-        //const answer =  "none";
         return "";
-        ;
+  
     }
-    //let answer = stack.join('');
-    console.log(answer)
     return answer;
 }
